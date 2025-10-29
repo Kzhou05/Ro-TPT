acffeaeaef3a8e3bdb7dbe9da40d55adf99a6e93
Kzhou05 <zhoukailovemonkey@gmail.com>
2025-10-29 16:47:47 +0800

    Update test_tpt_rotpt_fg.sh

--- scripts/test_tpt_rotpt_fg.sh
+++ scripts/test_tpt_rotpt_fg.sh
@@ -1,1 +1,16 @@
 
+#!/bin/bash
+
+data_root='./data/dataset'
+testsets=$1
+csv_loc='./log/test_otpt_finegrained_tp-ip-disp.csv'
+arch=RN50
+# arch=ViT-B/16
+bs=64
+ctx_init=a_photo_of_a
+run_type=tpt_otpt
+lambda_term=18
+
+python ./otpt_classification.py ${data_root} --test_sets ${testsets} --csv_log ${csv_loc} \
+-a ${arch} -b ${bs} --gpu 3 \
+--tpt --ctx_init ${ctx_init} --run_type ${run_type} --lambda_term ${lambda_term} \
